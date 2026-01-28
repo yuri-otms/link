f5a5233410b8d4b1289029fea9c51044f1af839a
yurio <monochro4s@gmail.com>
2026-01-28 09:44:50 +0900

    [ update ] コミックエッセイのブログ、個人サイトへのリンク追加

--- pages/src/App.tsx
+++ pages/src/App.tsx
@@ -35,6 +35,28 @@ function App() {
       ],
     },
     {
+      cardTitle: '身軽な生活を目指すブログ',
+      titleLink: 'https://yuri-l.hatenablog.com/',
+      description: 'なるべくものを減らしたり、デジタルミニマリストを目指したり、カフェインレス生活を目指したりのコミックエッセイ',
+      links: [],
+    },
+    {
+      cardTitle: '80歳まで続けるサイト',
+      titleLink: 'https://skskfun.com/',
+      description: '個人サイト。CMSで構築。過去のコミックエッセイ、ひきこもり脱出のヒント集など。',
+      links: [
+        {
+          linkTitle: '個人サイト開発日記',
+          linkUrl: 'https://skskfun.com/contents/log'
+        },
+
+        {
+          linkTitle: 'ひきこもりの時(コミックエッセイ)',
+          linkUrl: 'https://skskfun.com/contents/hiki'
+        }
+      ],
+    },
+    {
       cardTitle: 'サクッと作文　韓国語',
       titleLink: 'https://ko.skskfun.com/',
       description: '未経験就活時のポートフォリオ　韓国語を作文して学ぶWebアプリ(Python/Flask)',
